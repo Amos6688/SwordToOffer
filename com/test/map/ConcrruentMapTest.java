058cd7b4298ead0362b77fb211ca272e51a36278
Amos6688 <lclypyxf57@gmail.com>
2018-04-16 21:46:31 +0800

    剑指offer 面试题35相关题目3

---
+++ com/test/map/ConcrruentMapTest.java
@@ -0,0 +1,11 @@
+package com.test.map;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcrruentMapTest
+{
+    public static void main(String[] args)
+    {
+        ConcurrentHashMap<String, String> cchMap = new ConcurrentHashMap<>();
+    }
+}
